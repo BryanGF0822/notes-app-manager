69c81d7297d28f0d72a23432886b7f4796bbc418
Bryan Guapacha <bryangf0822@gmail.com>
2025-01-30 18:53:53 -0500

    Initial Commit

---
+++ Notes/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Notes
+//
+//  Created by Bryan Alexander Guapacha Florez on 30/01/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
