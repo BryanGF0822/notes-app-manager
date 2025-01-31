0d46eac0e72e151909b6a29943e317389821470b
Bryan Guapacha <bryangf0822@gmail.com>
2025-01-31 15:41:22 -0500

    Started the project and I am getting familiar with the SwiftUI framework.

--- Notes/ContentView.swift
+++ Notes/ContentView.swift
@@ -9,13 +9,66 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+//        Text("Nunca pares de aprender")
+//            .font(.system(size: 50))
+//            .lineLimit(2)
+//            .multilineTextAlignment(.trailing)
+//            .bold()
+//            .underline()
+//        
+//        Circle()
+//            .background(Color.blue)
+//            .padding()
+//            .background(Color.yellow)
+//            .frame(width: 200, height: 200)
+//        
+//        Rectangle()
+//            .frame(width: 200, height: 200)
+//            .foregroundStyle(Color.yellow)
+//            .cornerRadius(60)
+//            .opacity(0.5)
+//        
+//        Ellipse()
+//            .stroke(lineWidth: 5)
+//            .frame(width: 200, height: 400)
+//            .foregroundStyle(Color.blue)
+//            .rotationEffect(.degrees(30))
+        
+        ScrollView {
+            ForEach(0..<5) {index in
+                Button("Click here!") {
+                    // what the button does
+                }
+                .buttonStyle(.borderedProminent)
+                .tint(.red)
+                .controlSize(.large)
+                .buttonBorderShape(.capsule)
+                .disabled(false)
+            }
         }
-        .padding()
+        .frame(height:250)
+        .background(.brown)
+        
+        Button("Click here!") {
+            // what the button does
+        }
+        .buttonStyle(.borderedProminent)
+        .tint(.red)
+        .controlSize(.large)
+        .buttonBorderShape(.capsule)
+        .disabled(false)
+        
+//        Button(action: {
+//            // What the button does
+//        }) {
+//            Label("Profile", systemImage: "person.circle")
+//        }
+//        // All built-in modifiers can be applied in this case
+//        .buttonStyle(.borderedProminent)
+//        .tint(.gray)
+//        .controlSize(.large)
+//        .buttonBorderShape(.capsule)
+//        .disabled(false)
     }
 }
 
