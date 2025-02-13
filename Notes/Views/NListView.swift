98caeec46e1b6d502ba563984ac7f6f495335a40
Bryan Guapacha <bryangf0822@gmail.com>
2025-02-12 22:22:34 -0500

    Implemented button interactions and explored user input handling with TextField and TextEditor.

---
+++ Notes/Views/NListView.swift
@@ -0,0 +1,18 @@
+//
+//  NListView.swift
+//  Notes
+//
+//  Created by Bryan Alexander Guapacha Florez on 12/02/25.
+//
+
+import SwiftUI
+
+struct NListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NListView()
+}
